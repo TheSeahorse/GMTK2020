854797de37493f75d85363bf271fecd971b72779
Alexander Lahti <alexander.lahti.2359@student.uu.se>
2020-07-11 15:30:20 +0200

    can press q or space to navigate menues

--- FiftyFifty/Game/Level/Levels/Finish.gd
+++ FiftyFifty/Game/Level/Levels/Finish.gd
@@ -1,6 +1,8 @@
 extends Control
 
-
-
 func _on_Button_pressed() -> void:
 	get_tree().change_scene("res://Main.tscn")
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("jump_dash") or event.is_action_pressed("ui_select"):
+		_on_Button_pressed()
