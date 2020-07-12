34624c6b57369fbbbb324dd57ecdcea1c4296f9b
Mikael Mollberg <MikaelMollberg@users.noreply.github.com>
2020-07-12 19:10:42 +0200

    Add credits

--- FiftyFifty/Main.gd
+++ FiftyFifty/Main.gd
@@ -4,14 +4,29 @@ onready var Game = preload("res://Game/Game.tscn")
 
 var game
 var running = false
+var credits_visible = false
 
-func _on_StartGameButton_pressed():
-	game = Game.instance()
-	add_child(game)
-	running = true
-	$StartGameButton.hide()
+func _ready():
+	$Credits.hide()
 
 func _input(event: InputEvent) -> void:
 	if not running:
 		if event.is_action_pressed("enter"):
-			_on_StartGameButton_pressed()
+			if not credits_visible:
+				start_game()
+		if event.is_action_pressed("credits"):
+			if not credits_visible:
+				$Credits.show()
+				$VBoxContainer.hide()
+				credits_visible = true
+			else:
+				credits_visible = false
+				$Credits.hide()
+				$VBoxContainer.show()
+
+func start_game():
+	game = Game.instance()
+	add_child(game)
+	running = true
+	$VBoxContainer.hide()
+	$Credits.hide()
